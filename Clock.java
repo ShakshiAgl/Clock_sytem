28e62a1f2ebb005d925328ddf877efff9f89ee6e
unknown <agrawalsakshi836@gmail.com>
2023-05-17 10:17:31 +0545

    Clock_project

---
+++ Clock.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.awt.*;
+import java.text.*;
+import javax.swing.*;
+
+public class Clock extends JFrame {
+
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
+    SimpleDateFormat dateFormat;
+    JLabel timeLabel;
+    JLabel dayLabel;
+    JLabel dateLabel;
+    String time;
+    String day;
+    String date;
+
+    // constructor for frame
+    Clock() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("My Clock program");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 200);
+        this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
+        dateFormat = new SimpleDateFormat("MMMMM dd, yyyy");
+
+        timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Arial", Font.PLAIN, 58));
+        timeLabel.setForeground(new Color(255, 255, 255));
+        timeLabel.setBackground(Color.black);
+        timeLabel.setOpaque(true);
+
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Verdena", Font.PLAIN, 35));
+
+        dateLabel = new JLabel();
+        dateLabel.setFont(new Font("Verdena", Font.PLAIN, 27));
+
+        // getting the current time
+        time = timeFormat.format(Calendar.getInstance().getTime());
+
+        timeLabel.setText(time);
+
+        this.add(timeLabel);
+        this.add(dayLabel);
+        this.add(dateLabel);
+        this.setVisible(true);
+
+        setTime();
+    }
+
+    // method to set time
+    public void setTime() {
+        while (true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
+
+            date = dateFormat.format(Calendar.getInstance().getTime());
+            dateLabel.setText(date);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new Clock();
+    }
+
+}
